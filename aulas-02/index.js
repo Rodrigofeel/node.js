631feeae1d25a5c3634b3e2bc87801888f66071c
Rodrigo Felix <rodrigo-fellix@hotmail.com>
2025-10-12 23:43:26 -0300

    Update index.js

--- aulas-02/index.js
+++ aulas-02/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const server = express();
 
+server.get("/hello", (req, res) => {
+    return res.send("Hello World!");
+});
+
 server.listen(3000);
 
 //3000; 5000; 8000; 8080 portas comuns de serem usadas
