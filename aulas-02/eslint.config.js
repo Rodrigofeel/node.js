6f30553f254bb563d1baa595398b6327c2ed1fff
Rodrigo Felix <rodrigo-fellix@hotmail.com>
2025-10-17 10:30:16 -0300

    update

--- aulas-02/eslint.config.js
+++ aulas-02/eslint.config.js
@@ -3,6 +3,32 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    plugins: { js, react: require("eslint-plugin-react"), "@typescript-eslint": require("@typescript-eslint/eslint-plugin"), prettier: require("eslint-plugin-prettier") },
+    extends: [
+      "js/recommended",
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended"
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2021
+      },
+      parser: require.resolve("@typescript-eslint/parser")
+    },
+    rules: {
+      "prettier/prettier": "error"
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
 ]);
 
